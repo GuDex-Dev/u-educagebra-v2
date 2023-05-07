2ccbda7b61121321ee00cbcdd51473b978b85366
cristian10m <121249722+cristian10m@users.noreply.github.com>
2023-05-07 17:20:19 -0500

    le puse xd

--- src/Principal/FrmPerfiles.java
+++ src/Principal/FrmPerfiles.java
@@ -6,6 +6,7 @@ public class FrmPerfiles extends javax.swing.JFrame {
      * Creates new form FrmPerfiles
      */
     public FrmPerfiles() {
+        //xd
         //hola buenas
         initComponents();
         setLocationRelativeTo(null);
